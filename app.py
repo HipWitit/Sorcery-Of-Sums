40896f6c98821dfccddf647475cce1a5f3e1f89d
HipWitit <meshiabedard@gmail.com>
2026-02-28 00:25:23 -0800

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ st.markdown(f"""
         background-color: transparent !important;
         box-shadow: none !important;
         display: block !important;
-        margin: -120px auto 50 auto !important; 
+        margin: -120px auto 0 auto !important; 
         transition: transform 0.2s ease;
     }}
 
