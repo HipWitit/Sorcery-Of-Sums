31e96eaa4fa476b4fcd157f0dc72b2747255db59
HipWitit <meshiabedard@gmail.com>
2026-02-27 20:06:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,8 @@ st.markdown(f"""
 
     /* Resize and Center the "Enter your name" banner */
     img[src*="namefp.png"] {{
-        width: 1000px !important;   
+        width: 65% !important;
+        * UPDATE THIS FROM 65% TO 95% */
         display: block !important;
         margin-left: auto !important;
         margin-right: auto !important;
