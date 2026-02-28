c23acf8da60e753e551b544ced6cb87b5962d4fd
HipWitit <meshiabedard@gmail.com>
2026-02-28 00:28:25 -0800

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ st.markdown(f"""
         background-size: contain !important;
         background-repeat: no-repeat !important;
         background-position: center !important;
-        width: 300px !important;
+        width: 275px !important;
         height: 300px !important;
         border: none !important;
         background-color: transparent !important;
